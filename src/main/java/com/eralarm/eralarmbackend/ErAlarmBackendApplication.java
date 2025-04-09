6546c6f0c25f630bc8ed9797c727489db2b5fd8a
jewon <96jwjw@naver.com>
2025-04-09 18:12:11 +0900

    initial commit

---
+++ src/main/java/com/eralarm/eralarmbackend/ErAlarmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eralarm.eralarmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErAlarmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErAlarmBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eralarm/eralarmbackend/ErAlarmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eralarm.eralarmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErAlarmBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
